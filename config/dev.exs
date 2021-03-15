f0124e4bc894da6c5c2ce536f036e473d5370ad0
Rafael Evangelista <rafael.evans@Live.com>
2021-03-14 22:57:54 -0300

    Create user schema, endpoint to register user and handle the response

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,8 @@ config :rocketpay, Rocketpay.Repo,
   database: "rocketpay_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  port: 5432
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
